951d669321d83930ff1154c500694d3ea5e905de
Meghan Joseph <majoseph77@aol.com>
2015-07-28 09:30:51 -0700

    courses index working

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
 
+  get 'courses/index'
+
   root 'welcome#home'
 
   resources :users, only: [:new, :create, :edit]
   resource :session, only: [:new, :create, :destroy]
+  resources :courses
 
   get '/users/profile', to: 'users#show'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,10 +11,28 @@ bio = Course.create(:title => 'Biology', :num_sections => 43, :section_name => '
 
 cell = Section.create(:name => 'The Cell', :quiz => '1. What are the three basics of a cell?', :video_uri => 'W7cO2UKCrgY')
 carbon = Section.create(:name => 'Carbon', :quiz => '1. Question', :video_uri => 'dDBU0lg-HYE')
-
-
-
-binding.pry
-
 bio.sections << carbon
 
+
+
+
+anthro = Course.create(:title => 'Anthropology')
+chem = Course.create(:title => 'Chemistry')
+psych = Course.create(:title => 'Psychology')
+soci = Course.create(:title => 'Sociology')
+human = Course.create(:title => 'Humanities')
+engl = Course.create(:title => 'English')
+hist = Course.create(:title => 'History')
+bus = Course.create(:title => 'Business Management')
+chdev = Course.create(:title => 'Child Development')
+philo = Course.create(:title => 'Philosophy')
+envsci = Course.create(:title => 'Enviromental Science')
+astro = Course.create(:title => 'Astronomy')
+phys = Course.create(:title => 'Physics')
+micbio = Course.create(:title => 'Microbiology')
+geol = Course.create(:title => 'Geology')
+geogr = Course.create(:title => 'Geography')
+physio = Course.create(:title => 'Physiology')
+polisci = Course.create(:title => 'Political Science')
+anatomy = Course.create(:title => 'Anatomy')
+binding.pry
